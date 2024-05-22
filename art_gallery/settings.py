07e43d3fe5fd2d0d0041d40bc7e2bf8a9a657449
Abu Naim <abunaim@Abus-Air-2.lan>
2024-05-22 23:37:58 +0600

    done

--- art_gallery/settings.py
+++ art_gallery/settings.py
@@ -139,25 +139,25 @@ WSGI_APPLICATION = 'art_gallery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "art_gallery_db",
-        'USER': "postgres",
-        'PASSWORD': 1234,
-        'HOST': "localhost",
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
-DATABASES['default'] = dj_database_url.parse("postgres://artgallarydb_user:cc64Bo80aPHfLMpUDZ9nIYINSGmkuE14@dpg-cp721ki0si5c73amr57g-a.oregon-postgres.render.com/artgallarydb")
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': "art_gallery_db",
+#         'USER': "postgres",
+#         'PASSWORD': 1234,
+#         'HOST': "localhost",
+#         'PORT': 5432,
+#     }
+# }
+
+# DATABASES['default'] = dj_database_url.parse("postgres://artgallarydb_user:cc64Bo80aPHfLMpUDZ9nIYINSGmkuE14@dpg-cp721ki0si5c73amr57g-a.oregon-postgres.render.com/artgallarydb")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
